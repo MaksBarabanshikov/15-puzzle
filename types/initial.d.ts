c8ff62a8f037f800ce46cca5473970a5b6e1db8a
MaksKilla <maksimbarabanshikov2001@gmail.com>
2022-09-17 15:03:28 +0400

    Updates

--- types/initial.d.ts
+++ types/initial.d.ts
@@ -2,3 +2,7 @@ export declare type ISize = number;
 export declare type IField = number[];
 export declare type IMatrix = number[][];
 export declare type IFieldSize = number;
+export interface IPosTile {
+    x: number;
+    y: number;
+}
