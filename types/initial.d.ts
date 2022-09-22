1ef5a42d80792c20f92a816c8617e2ae7bc36b10
MaksimBarabanshikov <maksimbarabanshikov2001@gmail.com>
2022-09-22 22:30:37 +0400

    Updates

--- types/initial.d.ts
+++ types/initial.d.ts
@@ -1,8 +1,9 @@
 export declare type ISize = number;
 export declare type IField = number[];
-export declare type IMatrix = number[][];
+export declare type IMatrix = Coordinate[][];
 export declare type IFieldSize = number;
 export interface IPosTile {
-    x: number;
-    y: number;
+    x: Coordinate;
+    y: Coordinate;
 }
+export declare type Coordinate = number;
