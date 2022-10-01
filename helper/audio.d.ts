3477ab54b8a0e1e90a61d6f2de7c127cbc54312e
MaksimBarabanshikov <maksimbarabanshikov2001@gmail.com>
2022-10-01 16:36:31 +0400

    Updates

---
+++ helper/audio.d.ts
@@ -0,0 +1,3 @@
+export declare const swapAudio: () => void;
+export declare const currentTileAudio: () => void;
+export declare const currentPlayAudio: (oldCurrentTile: any, newCurrentTile: any) => void;
